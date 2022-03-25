29bc1b8c9909157c513aa99c58b550d7b6254f9d
Nagendra121 <nagendra.singh.she@gmail.com>
2022-03-25 16:12:48 +0530

    Nike

---
+++ page.js
@@ -0,0 +1,8 @@
+function imgSlider(anthing){
+    document.querySelector('.pics').src = anthing;
+
+}
+function changeBgColor(color){
+    const sec= document.querySelector('.sec');
+    sec.style.background= color
+}
